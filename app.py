eaaf250a1aa45f6f74370e76bcd89291209b96fe
Ridho Alhafizhi <36652719+ralhafizhi@users.noreply.github.com>
2021-06-02 20:19:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,10 @@ def prediksi():
                            prob0=prob0, prob1=prob1, prob2=prob2, prob3=prob3, prob4=prob4, prob5=prob5,
                            total=total, image_location=image_location)
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5000")
+    app.debug = True
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port, debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0", port="5000")
     # app.run(debug=True)
